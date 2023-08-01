a14b64dfc5212d7004a6220e69d0b26c314d489b
trapala85mx <rtrapala.dev.ds@gmail.com>
2023-07-31 18:00:53 -0600

    Adding Timeframes Enum

---
+++ utils/enums/timeframes.py
@@ -0,0 +1,23 @@
+# Python
+from enum import Enum
+# Project
+# Externals
+
+
+class Timeframes(Enum):
+    
+    TIMEFRAME_1MONT = "1M"
+    TIMEFRAME_1WEEK = "1w"
+    TIMEFRAME_3DAYS = "3d"
+    TIMEFRAME_1DAY = "1d"
+    TIMEFRAME_12HOUR = "12h"
+    TIMEFRAME_8HOUR = "8h"
+    TIMEFRAME_6HOUR = "6h"
+    TIMEFRAME_4HOUR = "4h"
+    TIMEFRAME_2HOUR = "2h"
+    TIMEFRAME_1HOUR = "1h"
+    TIMEFRAME_30minutes = "30m"
+    TIMEFRAME_15minutes = "15m"
+    TIMEFRAME_5minutes = "5m"
+    TIMEFRAME_3minutes = "3m"
+    TIMEFRAME_1minute = "1m"
